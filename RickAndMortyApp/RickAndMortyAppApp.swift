3402785ba387952d17950e41747136a4c577f374
zahraAfa <anisafatimahazzahra@gmail.com>
2023-11-14 09:49:27 +0700

    Initial Commit

---
+++ RickAndMortyApp/RickAndMortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyAppApp.swift
+//  RickAndMortyApp
+//
+//  Created by Anisa Fatimah Azzahra on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
